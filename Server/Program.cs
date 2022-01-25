04e8113026d8bc6813173f778743f21b16cda669
mbugowski1 <mbugowski1@wp.pl>
2022-01-25 11:45:03 +0100

    Created Topshelf service

--- Server/Program.cs
+++ Server/Program.cs
@@ -1,4 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-using System;
-
-Console.WriteLine("Hello, World!");
+﻿using System;
+using Topshelf;
+namespace Server
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            var rc = HostFactory.Run(x =>
+            {
+                x.Service<Service>(s =>
+                {
+                    s.ConstructUsing(name => new Service());
+                    s.WhenStarted(tc => tc.Start());
+                    s.WhenStopped(tc => tc.Stop());
+                });
+                x.RunAsLocalSystem();
+                x.SetDescription("Vacation Manager Service");
+                x.SetDisplayName("Vacation Manager Service");
+                x.SetServiceName("VacationManager");
+            });
+            var exitCode = (int)Convert.ChangeType(rc, rc.GetTypeCode());
+            Environment.ExitCode = exitCode;
+        }
+    }
+}
