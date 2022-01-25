69015f57bbbead34eb168ddc4987eb9663784bde
mbugowski1 <mbugowski1@wp.pl>
2022-01-25 15:09:30 +0100

    Moved networking

---
+++ Server/Network.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server
+{
+    public class Network
+    {
+        private readonly List<Socket> _clients = new();
+        private readonly Socket _socket = new(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        private readonly byte[] _buffer = new byte[2048];
+        private readonly int port;
+        public Network(int port) => this.port = port;
+        public void Open()
+        {
+            _socket.Bind(new IPEndPoint(IPAddress.Any, port));
+            _socket.Listen(5);
+            _socket.BeginAccept(AcceptCallback, null);
+        }
+        private void AcceptCallback(IAsyncResult AR)
+        {
+            Socket client = _socket.EndAccept(AR);
+            _clients.Add(client);
+            client.BeginReceive(_buffer, 0, _buffer.Length, SocketFlags.None, ReceiveCallback, client);
+            Debug.WriteLine("Client connected");
+            _socket.BeginAccept(AcceptCallback, null);
+        }
+        private void ReceiveCallback(IAsyncResult AR)
+        {
+            Socket? client = (Socket?)AR.AsyncState;
+            if (client == null) return;
+            int received;
+            try
+            {
+                received = client.EndReceive(AR);
+            }
+            catch(SocketException)
+            {
+                Debug.WriteLine("Client disconnected");
+                _clients.Remove(client);
+                return;
+            }
+            string recText = Encoding.UTF8.GetString(_buffer, 0, received);
+            client.BeginReceive(_buffer, 0, _buffer.Length, SocketFlags.None, ReceiveCallback, client);
+        }
+        public void Close()
+        {
+            foreach (var client in _clients)
+            {
+                client.Shutdown(SocketShutdown.Both);
+                client.Close();
+            }
+            _socket.Close();
+        }
+    }
+}
--- Server/Service.cs
+++ Server/Service.cs
@@ -14,43 +14,26 @@ namespace Server
     public class Service
     {
         private volatile bool _working;
-        private List<Socket> _clients = new();
-        private Socket _socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
-        private byte[] _buffer = new byte[2048];
-        public Service()
+        public bool Working
         {
-            
+            get => _working;
+            private set => _working = value;
         }
+        private Thread? _runningService;
+        private readonly Network _network = new(1337);
         public void Start()
         {
-            _working = true;
-            new Thread(new ThreadStart(Run)).Start();
             Debug.WriteLine("Starting Server");
+            _runningService = new Thread(new ThreadStart(Run));
+            _runningService.Start();
         }
         public void Stop()
         {
-            _working = false;
+            Working = false;
         }
-        public void Run()
+        private void Run()
         {
-            _socket.Bind(new IPEndPoint(IPAddress.Any, 1337));
-            _socket.Listen(5);
-            _socket.BeginAccept(AcceptCallback, null);
-        }
-        private void AcceptCallback(IAsyncResult AR)
-        {
-            Socket client = _socket.EndAccept(AR);
-            _clients.Add(client);
-            client.BeginReceive(_buffer, 0, _buffer.Length, SocketFlags.None, ReceiveCallback, client);
-        }
-        private void ReceiveCallback(IAsyncResult AR)
-        {
-            Socket? client = (Socket?)AR.AsyncState;
-            if (client == null) return;
-            int received = client.EndReceive(AR);
-            string recText = Encoding.UTF8.GetString(_buffer, 0, received);
-            client.BeginReceive(_buffer, 0, _buffer.Length, SocketFlags.None, ReceiveCallback, client);
-            //client.Close();
+            _network.Open();
         }
     }
 }
