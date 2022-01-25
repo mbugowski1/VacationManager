f132f2aac2364d2406073d01e6a675524893b0a6
mbugowski1 <mbugowski1@wp.pl>
2022-01-25 17:33:31 +0100

    Added DataReceived test

--- VacationManagerServerTests/NetworkTests.cs
+++ VacationManagerServerTests/NetworkTests.cs
@@ -3,6 +3,8 @@ using System.Net.Sockets;
 using System.Net;
 using Server;
 using System.Collections.Generic;
+using System.Text;
+using System;
 
 namespace VacationManagerServerTests
 {
@@ -11,21 +13,65 @@ namespace VacationManagerServerTests
         [Fact]
         public void Connecting_ToServer_Connected()
         {
-            Network service = new(1000);
+            ushort port = 1000;
+            Network service = new(port);
             service.Open();
 
             Socket socket = new(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
-            socket.Connect(IPAddress.Loopback, 1000);
+            socket.Connect(IPAddress.Loopback, port);
 
             Assert.True(socket.Connected);
             socket.Close();
             service.Close();
         }
+        [Fact]
+        public void ReceivingData_Server_GetString()
+        {
+            // Arrange
+            ushort port = 1001;
+            Network service = new(port);
+            DataArgs data = null;
+            bool wait = true;
+            service.dataReceived += (sender, e) =>
+            {
+                data = e;
+                wait = false;
+            };
+            Socket socket = new(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            string user = "Unknown";
+            string message = "test";
+            byte[] send = Encoding.UTF8.GetBytes(message);
+
+            // Act
+            service.Open();
+            socket.Connect(IPAddress.Loopback, port);
+            socket.Send(send);
+            while (wait) ;
+
+            // Assert
+            Assert.Equal(user, data.Username);
+            Assert.Equal(message, data.Data);
+        }
+        [Fact]
+        public void Disconnecting_FromServer_Disconnected()
+        {
+            ushort port = 1002;
+            Network service = new(port);
+            service.Open();
+
+            Socket socket = new(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            socket.Connect(IPAddress.Loopback, port);
+
+            socket.Close();
+          
+            Assert.True(true);
+            service.Close();
+        }
         [Theory]
-        [InlineData(0, 1001)]
-        [InlineData(1, 1002)]
-        [InlineData(2, 1003)]
-        public void Disconnecting_FromServer_Disconnected(short userCount, ushort port)
+        [InlineData(0, 1003)]
+        [InlineData(1, 1004)]
+        [InlineData(2, 1005)]
+        public void ShuttingDown_Server(short userCount, ushort port)
         {
             Network service = new(port);
             service.Open();
@@ -38,9 +84,9 @@ namespace VacationManagerServerTests
                 users.Add(user);
             }
 
-            service.Close();
+            var exception = Record.Exception(() => service.Close());
             users.ForEach(x => x.Close());
-            Assert.True(true);
+            Assert.Null(exception);
         }
     }
 }
