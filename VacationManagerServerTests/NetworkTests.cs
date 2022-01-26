ae6b9f157d953bce3a82683d19c79120ebd8d30e
mbugowski1 <mbugowski1@wp.pl>
2022-01-26 15:45:51 +0100

    Added sendData test

--- VacationManagerServerTests/NetworkTests.cs
+++ VacationManagerServerTests/NetworkTests.cs
@@ -51,6 +51,10 @@ namespace VacationManagerServerTests
             // Assert
             Assert.Equal(user, data.Username);
             Assert.Equal(message, data.Data);
+
+            service.Close();
+            socket.Shutdown(SocketShutdown.Both);
+            socket.Close();
         }
         [Fact]
         public void Disconnecting_FromServer_Disconnected()
@@ -87,6 +91,46 @@ namespace VacationManagerServerTests
             var exception = Record.Exception(() => service.Close());
             users.ForEach(x => x.Close());
             Assert.Null(exception);
+        }
+        [Fact]
+        public void SendingData_FromServer_SendString()
+        {
+            // Assign
+            ushort port = 1006;
+            Network service = new(port);
+            Socket socket = new(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            string message = "test";
+            string expected = "test echo";
+            byte[] buffer = new byte[2048];
+
+            // Act
+            service.Open();
+            socket.Connect(IPAddress.Loopback, port);
+            socket.Send(Encoding.UTF8.GetBytes(message));
+            socket.Receive(buffer);
+            string received = Encoding.UTF8.GetString(buffer);
+            received = received.Substring(0, received.IndexOf('\0'));
+
+            // Assert
+            Assert.Equal(expected, received);
+            socket.Close();
+            service.Close();
+        }
+        [Fact]
+        public void SendingData_FromServer_GetEvent()
+        {
+            // Assign
+            ushort port = 1007;
+            Network service = new(port);
+            Socket socket = new(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            string message = "test";
+
+            // Act
+            service.Open();
+            socket.Connect(IPAddress.Loopback, port);
+
+            // Assert
+            Assert.Raises<DataArgs>(handler => service.dataSent += handler, handler => service.dataSent -= handler, () => service.SendMessage(null, message));
         }
     }
 }
