6ef34e5136b16389c3e4d87492d5480a193d8707
mbugowski1 <mbugowski1@wp.pl>
2022-02-02 06:19:52 +0100

    Added user creation

--- Navigation Drawer App/Quick.cs
+++ Navigation Drawer App/Quick.cs
@@ -9,17 +9,7 @@ namespace Navigation_Drawer_App
 {
     internal static class Quick
     {
-        public static void RegisterUser(string username, string password, string firstname, string lastname, string position, string supervisor)
-        {
-            var message = new Message();
-            message.Operation = Message.Code.AddUser;
-            string[] data = new string[] { username, password, firstname, lastname, position };
-            message.Data = Serializer.Serialize(data);
-            Globals.Connection.SendMessage(Serializer.Serialize(message));
-            data = new string[] { username, supervisor };
-            message.Operation = Message.Code.NewSupervisor;
-            message.Data = Serializer.Serialize(data);
-            Globals.Connection.SendMessage(Serializer.Serialize(message));
-        }
+        
+
     }
 }
--- Navigation Drawer App/Register.xaml.cs
+++ Navigation Drawer App/Register.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MaterialDesignThemes.Wpf;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,6 +12,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using VacationManagerLibrary;
 
 namespace Navigation_Drawer_App
 {
@@ -19,9 +21,63 @@ namespace Navigation_Drawer_App
     /// </summary>
     public partial class Register : Window
     {
-        public Register()
+        private LoginScreen parent;
+        private MainWindow mainWindow;
+        public Register(LoginScreen loginScreen, MainWindow mainWindow)
         {
             InitializeComponent();
+            parent = loginScreen;
+            this.mainWindow = mainWindow;
+        }
+
+        private void btnSubmit_click(object sender, RoutedEventArgs e)
+        {
+            RegisterUser(txtUsername.Text, txtPassword.Password, Firstname.Text, Surname.Text, Position.Text, Supervisor.Text);
+            mainWindow.Show();
+            Close();
+        }
+        private bool RegisterUser(string username, string password, string firstname, string lastname, string position, string supervisor)
+        {
+            var message = new Message();
+            message.Operation = Message.Code.AddUser;
+            string[] data = new string[] { username, password, firstname, lastname, position };
+            message.Data = Serializer.Serialize(data);
+            Globals.Connection.SendMessage(Serializer.Serialize(message));
+            data = new string[] { username, supervisor };
+            message.Operation = Message.Code.NewSupervisor;
+            message.Data = Serializer.Serialize(data);
+            Globals.Connection.SendMessage(Serializer.Serialize(message));
+            return true;
+        }
+        public bool isDarkTheme { get; set; }
+
+        private readonly PaletteHelper paletteHelper = new PaletteHelper();
+        private void toggleTheme(object sender, RoutedEventArgs e)
+        {
+            ITheme theme = paletteHelper.GetTheme();
+
+            if (isDarkTheme = theme.GetBaseTheme() == BaseTheme.Dark)
+            {
+                isDarkTheme = false;
+                theme.SetBaseTheme(Theme.Light);
+            }
+            else
+            {
+                isDarkTheme = true;
+                theme.SetBaseTheme(Theme.Dark);
+            }
+            paletteHelper.SetTheme(theme);
+        }
+
+        private void exitApp(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        protected override void OnMouseLeftButtonDown(MouseButtonEventArgs e)
+        {
+            base.OnMouseLeftButtonDown(e);
+            DragMove();
         }
     }
 }
